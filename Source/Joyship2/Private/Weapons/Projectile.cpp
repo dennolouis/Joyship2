65cbfdbebb11fde2fca00b95f338b8db26ab6cea
Denno Louis <dennolouis1@gmail.com>
2026-03-11 21:22:28 -0400

    player projectile can damage turret now

--- Source/Joyship2/Private/Weapons/Projectile.cpp
+++ Source/Joyship2/Private/Weapons/Projectile.cpp
@@ -3,7 +3,6 @@
 #include "GameFramework/ProjectileMovementComponent.h"
 #include "GameFramework/DamageType.h"
 #include "Kismet/GameplayStatics.h"
-#include "Components/HealthComponent.h"
 
 AProjectile::AProjectile()
 {
@@ -15,6 +14,7 @@ AProjectile::AProjectile()
     RootComponent = CollisionComp;
 
     CollisionComp->OnComponentHit.AddDynamic(this, &AProjectile::OnHit);
+    CollisionComp->OnComponentBeginOverlap.AddDynamic(this, &AProjectile::OnOverlap);
 
     ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovement"));
     ProjectileMovement->UpdatedComponent = CollisionComp;
@@ -36,18 +36,27 @@ void AProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimi
 {
     if (OtherActor && OtherActor != this && OtherComp)
     {
-        // If the other actor has a health component, apply damage to it directly
-        UHealthComponent* HC = OtherActor->FindComponentByClass<UHealthComponent>();
-        if (HC)
-        {
-            HC->ApplyDamage(Damage);
-        }
-        else
-        {
-            // Fallback to gameplay damage system
-            UGameplayStatics::ApplyPointDamage(OtherActor, Damage, GetVelocity(), Hit, GetInstigatorController(), this, UDamageType::StaticClass());
-        }
+        if (ProjectileMovement) ProjectileMovement->StopMovementImmediately();
+
+        UGameplayStatics::ApplyDamage(OtherActor, Damage, GetInstigatorController(), this, UDamageType::StaticClass());
     }
 
     Destroy();
+}
+
+void AProjectile::OnOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+    UE_LOG(LogTemp, Warning, TEXT("[Projectile] OnOverlap called Other=%s Comp=%s"), OtherActor ? *OtherActor->GetName() : TEXT("None"), OtherComp ? *OtherComp->GetName() : TEXT("None"));
+    if (OtherActor && OtherActor != this && OtherComp)
+    {
+        if (OtherComp->GetCollisionEnabled() == ECollisionEnabled::QueryOnly)
+        {
+            UE_LOG(LogTemp, Verbose, TEXT("[Projectile] Overlap ignored: OtherComp is QueryOnly"));
+            return;
+        }
+
+        if (ProjectileMovement) ProjectileMovement->StopMovementImmediately();
+        UGameplayStatics::ApplyDamage(OtherActor, Damage, GetInstigatorController(), this, UDamageType::StaticClass());
+        Destroy();
+    }
 }
